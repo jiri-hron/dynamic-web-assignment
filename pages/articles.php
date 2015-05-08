8c266c1fdd2a4a12352f869d70f625a6d5403a06
Jiri Hron <jiri.m.hron@gmail.com>
2015-05-08 05:46:01 -0700

    initial commit

---
+++ pages/articles.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Jeremy Clarkson (Orangutan) - used to be Top Gear presenter on BBC2</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/main.css" type="text/css">
+    <!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+    <div id="main">
+        <div id="common">
+            <div id="logo">
+                <img src="../img/tg_logo.png" alt="Top Gear">
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="../index.html">Top Gear Show</a>
+                    </li>
+                    <li>
+                        <a href="clarkson.html">Jeremy Clarkson</a>
+                    </li>
+                    <li>
+                        <a href="may.html">James May</a>
+                    </li>
+                    <li>
+                        <a href="hammond.html">Richard Hammond</a>
+                    </li>
+                    <li>
+                        <a href="stig.html">The Stig</a>
+                    </li>
+                    <li class="selected">
+                        <a href="articles.php">Articles</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <article>
+
+        	<?php
+				
+				echo 'hello';
+			?>
+            
+            <footer>
+                <span>author: Jiř&iacute; Hron</span>
+            </footer>
+        </article>
+    </div>
+</body>
---
+++ php/psl-config.php
@@ -0,0 +1,11 @@
+<?php
+	define("HOST", "localhost");     // The host you want to connect to.
+	define("USER", "reader");    // The database username. 
+	define("PASSWORD", "2SuFg3nakppTWPMH");    // The database password. 
+	define("DATABASE", "secure_login");    // The database name.
+	 
+	define("CAN_REGISTER", "any");
+	define("DEFAULT_ROLE", "member");
+	 
+	define("SECURE", TRUE); 
+?>
